b16aae8327baa48cadb488c3938ccc2386ff92f8
Alex Sinagin <a.sinyagin996@gmail.com>
2024-02-19 21:56:16 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num%10;
+int num2 = num/100;
+Console.WriteLine($"Сумма {num1} и {num2} равна {num1 + num2}");
